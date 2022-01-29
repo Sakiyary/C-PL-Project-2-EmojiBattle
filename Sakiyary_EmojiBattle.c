7396703325435f045170d3816b3e7976dee74b57
Sakiyary <sakiyary19@qq.com>
2022-01-29 13:56:33 +0800

    ChargeEnemy and Level system

--- Sakiyary_EmojiBattle.c
+++ Sakiyary_EmojiBattle.c
@@ -1,6 +1,15 @@
 ﻿#include"Sakiyary_EmojiBattle.h"
 
 int main() {
+    InitAll();
+    LoadRes();
+    PrintThemeBG();
+    const Uint8 *KeyValue = SDL_GetKeyboardState(NULL);
+    ThemeUI(&TotleEvent, KeyValue);
+    return 0;
+}
+
+void InitAll() {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
     SDL_EventState(SDL_MOUSEMOTION, SDL_IGNORE);
     SDL_EventState(SDL_MOUSEBUTTONDOWN, SDL_IGNORE);
@@ -12,21 +21,19 @@ int main() {
     Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED);
     Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 2048);
     srand((unsigned) time(NULL));
-    Load();
-    const Uint8 *KeyValue = SDL_GetKeyboardState(NULL);
-    InitialUI(&TotleEvent, KeyValue);
-    return 0;
 }
 
-void InitialUI(SDL_Event *event, const Uint8 *KeyValue) {
+void ThemeUI(SDL_Event *event, const Uint8 *KeyValue) {
     while (1) {
-        while (SDL_WaitEvent(event)) {
+        PrintThemeBG();
+        while (SDL_WaitEventTimeout(event, mSPF)) {
             if (event->type == SDL_QUIT || KeyValue[SDL_SCANCODE_ESCAPE])
                 return;
             if (KeyValue[SDL_SCANCODE_RETURN] || KeyValue[SDL_SCANCODE_KP_ENTER]) {
                 printf("Game Start!\n");
                 if (!GameUI(event, KeyValue))
                     return;
+                PrintThemeBG();
             }
         }
     }
@@ -34,14 +41,18 @@ void InitialUI(SDL_Event *event, const Uint8 *KeyValue) {
 
 int GameUI(SDL_Event *event, const Uint8 *KeyValue) {
     Mix_PlayMusic(BGM, -1);
+    Upgrade();
     while (1) {
-        Cooldown();
+        CoolDown();
+        if (!LevelCD)
+            CreateEnemy(Level);
+        Move();
         PrintAnime();
         if (MyAction(GetKey(KeyValue))) {
             Mix_PauseMusic();
             return 1;
         }
-        SDL_Delay(KDFPS);
+        SDL_Delay(mSPF);
         while (SDL_PollEvent(event))
             if (event->type == SDL_QUIT || KeyValue[SDL_SCANCODE_ESCAPE])
                 return 0;
@@ -51,52 +62,59 @@ int GameUI(SDL_Event *event, const Uint8 *KeyValue) {
 int GetKey(const Uint8 *KeyValue) {
     int Input = 0;
     if (KeyValue[SDL_SCANCODE_UP] || KeyValue[SDL_SCANCODE_W])
-        Input |= 0x1;
+        Input |= MoveUp;
     if (KeyValue[SDL_SCANCODE_DOWN] || KeyValue[SDL_SCANCODE_S])
-        Input |= 0x2;
+        Input |= MoveDown;
     if (KeyValue[SDL_SCANCODE_LEFT] || KeyValue[SDL_SCANCODE_A])
-        Input |= 0x4;
+        Input |= MoveLeft;
     if (KeyValue[SDL_SCANCODE_RIGHT] || KeyValue[SDL_SCANCODE_D])
-        Input |= 0x8;
+        Input |= MoveRight;
     if (KeyValue[SDL_SCANCODE_LSHIFT] || KeyValue[SDL_SCANCODE_RSHIFT])
-        Input |= 0x10;
+        Input |= Shoot;
     if (KeyValue[SDL_SCANCODE_COMMA])
-        Input |= 0x20;
+        Input |= RotateLeft;
     if (KeyValue[SDL_SCANCODE_PERIOD])
-        Input |= 0x40;
+        Input |= RotateRight;
     if (KeyValue[SDL_SCANCODE_SLASH])
-        Input |= 0x80;
-    if (KeyValue[SDL_SCANCODE_BACKSPACE])Input |= 0x800;
+        Input |= RotateReset;
+    if (KeyValue[SDL_SCANCODE_SPACE])
+        Input |= Pause;
+    if (KeyValue[SDL_SCANCODE_TAB])
+        Input |= Tab;
+    if (KeyValue[SDL_SCANCODE_BACKSPACE])
+        Input |= Back2Theme;
     return Input;
 }
 
 int MyAction(int Input) {
-    if (Input & 0x1 && My.FRect.y >= My.FSpeed)
+    if (Input & MoveUp && My.FRect.y >= 0)
         My.FRect.y -= My.FSpeed;
-    if (Input & 0x2 && My.FRect.y <= Height - My.FSpeed - My.FRect.h)
+    if (Input & MoveDown && My.FRect.y <= Height - My.FRect.h)
         My.FRect.y += My.FSpeed;
-    if (Input & 0x4 && My.FRect.x >= My.FSpeed)
+    if (Input & MoveLeft && My.FRect.x >= 0)
         My.FRect.x -= My.FSpeed;
-    if (Input & 0x8 && My.FRect.x <= Width - My.FSpeed - My.FRect.w)
+    if (Input & MoveRight && My.FRect.x <= Width - My.FRect.w)
         My.FRect.x += My.FSpeed;
-    if (Input & 0x10 && MyBulletCD == 0) {
-        ListPush(&MyBullets, CreateBullet(My, 0x10, MyBulletA, 20, My.Direction, 10));
-        ListPush(&MyBullets, CreateBullet(My, 0x10, MyBulletA, 20, My.Direction + 20, 10));
-        ListPush(&MyBullets, CreateBullet(My, 0x10, MyBulletA, 20, My.Direction - 20, 10));
-        MyBulletCD = 10;
+    if (Input & Shoot && MyBulletCD == 0) {
+        ListAdd(&MyBullets, CreateObject(My, MyBltA, 20, My.Direction, 1, MyBltType, 0x00, MyBltDmg));
+        ListAdd(&MyBullets, CreateObject(My, MyBltA, 20, My.Direction - 15, 1, MyBltType, 0x00, MyBltDmg));
+        ListAdd(&MyBullets, CreateObject(My, MyBltA, 20, My.Direction + 15, 1, MyBltType, 0x00, MyBltDmg));
+        ListAdd(&MyBullets, CreateObject(My, MyBltA, 20, My.Direction - 30, 1, MyBltType, 0x00, MyBltDmg));
+        ListAdd(&MyBullets, CreateObject(My, MyBltA, 20, My.Direction + 30, 1, MyBltType, 0x00, MyBltDmg));
+        MyBulletCD = 15;
     }
-    if (Input & 0x20 && My.Direction < 270)
+    if (Input & RotateLeft && My.Direction < 270)
         My.Direction += 10;
-    if (Input & 0x40 && My.Direction > 90)
+    if (Input & RotateRight && My.Direction > 90)
         My.Direction -= 10;
-    if (Input & 0x80 && My.Direction != 180)
+    if (Input & RotateReset && My.Direction != 180)
         My.Direction = 180;
-    if (Input & 0x800)
+    if (Input & Back2Theme)
         return 1;
     return 0;
 }
 
-void ListPush(OP **List, OP *NewNode) {
+void ListAdd(OP **List, OP *NewNode) {
     if (*List == NULL) {
         *List = NewNode;
         return;
@@ -107,98 +125,302 @@ void ListPush(OP **List, OP *NewNode) {
     Now->Next = NewNode;
 }
 
-OP *CreateBullet(OP who, int type, float size, float fspeed, double direction, int damage) {
+OP *CreateObject(OP who, float size, float fspeed, double direction, int hp, int type, int status, int damage) {
     OP *New = malloc(sizeof(OP));
-    New->FRect.x = who.FRect.x + who.FCentre.x - size / 2;
-    New->FRect.y = who.FRect.y + who.FCentre.y - size / 2;
     New->FRect.w = size;
     New->FRect.h = size;
     New->FCentre.x = size / 2;
     New->FCentre.y = size / 2;
+    if (type & (MyBltType | L2BltType | L3Blt1Type | L3Blt2Type)) {
+        New->FRect.x = who.FRect.x + who.FCentre.x - size / 2;
+        New->FRect.y = who.FRect.y + who.FCentre.y - size / 2;
+        New->Direction = direction;
+    } else if (type & (L1EType | L2E2Type | L3EType)) {
+        New->FRect.x = RorL > 1 ? (Width - size) / 2
+                                : RorL ? -size + (float) (rand() % ChargeRandRage)
+                                       : Width - (float) (rand() % ChargeRandRage);
+        New->FRect.y = -size;
+        if (type & L1EType)
+            RorL += RorL > 1 ? -RorL : 1;
+        else
+            RorL = !RorL;
+        double FollowDir = XY2Dir(New->FRect.x + New->FCentre.x,
+                                  New->FRect.y + 0,
+                                  who.FRect.x + who.FCentre.x,
+                                  who.FRect.y + who.FRect.h);
+        New->Direction = fabs(FollowDir) > 15 ?
+                         FollowDir > 0 ? 15 : -15 : FollowDir;
+    }
     New->FSpeed = fspeed;
-    New->Direction = direction;
-    New->HP = 1;
+    New->HP = hp;
     New->Type = type;
-    New->Status = 0x0;
+    New->Status = status;
     New->Damage = damage;
     New->Next = NULL;
     return New;
 }
 
-void BulletListMove(OP **BulletList) {
-    if (*BulletList == NULL)
+void ListMove(OP **List) {
+    if (*List == NULL)
         return;
-    OP *Now = *BulletList;
+    OP *Now = *List;
     while (Now != NULL) {
+        if (Now->Type & (L2BltType | L1EType | L2E2Type | L3EType)) {
+            double dDir = Now->Type & L1EType ? 0.5 : 0.2;
+            double FollowDir = XY2Dir(Now->FRect.x + Now->FCentre.x,
+                                      Now->FRect.y + 0,
+                                      My.FRect.x + My.FCentre.x,
+                                      My.FRect.y + My.FRect.h);
+            Now->Direction += FollowDir - Now->Direction > 1 ? dDir : -dDir;
+        }
         Now->FRect.x += Now->FSpeed * (float) sin(Now->Direction * D2R);
         Now->FRect.y += Now->FSpeed * (float) cos(Now->Direction * D2R);
-        if ((Now->FRect.y <= -Now->FRect.h)
+        if ((Now->FRect.y < -Now->FRect.h - 10)
             || (Now->FRect.y >= Height)
-            || (Now->FRect.x <= -Now->FRect.w)
-            || (Now->FRect.x >= Width))
+            || (Now->FRect.x < -Now->FRect.w - 10)
+            || (Now->FRect.x >= Width + 10)) {
             Now->HP = 0;
+            if (Now->Type != MyBltType)
+                My.HP -= Now->Damage / 2;
+        }
         Now = Now->Next;
     }
-    BulletListRemove(BulletList);
+    Now = *List;
+    if (Now->Type != MyBltType) {
+        Collision(List);
+        ListRemove(&MyBullets);
+    }
+    ListRemove(List);
 }
 
-void BulletListRemove(OP **BulletList) {
-    if (*BulletList == NULL)
+void Collision(OP **List) {
+    if (*List == NULL)
         return;
-    OP *Now = *BulletList;
+    OP *Now = *List;
+    while (Now != NULL) {
+        for (OP *i = MyBullets; i != NULL; i = i->Next)
+            if (XY2Dis(i->FRect.x + i->FCentre.x,
+                       i->FRect.y + i->FCentre.x,
+                       Now->FRect.x + Now->FCentre.x,
+                       Now->FRect.y + Now->FCentre.y) < i->FRect.w + Now->FRect.w / 3) {
+                if (Now->HP)
+                    i->HP = 0;
+                Now->HP -= Now->HP - i->Damage < 0 ? Now->HP : i->Damage;
+            }
+
+        if (!Now->HP) {
+            //Props
+        } else if (XY2Dis(My.FRect.x + My.FCentre.x,
+                          My.FRect.y + My.FCentre.y,
+                          Now->FRect.x + Now->FCentre.x,
+                          Now->FRect.y + Now->FCentre.y) < My.FRect.w) {
+            My.HP -= Now->Damage;
+            Now->HP = 0;
+        }
+        Now = Now->Next;
+    }
+}
+
+void ListRemove(OP **List) {
+    if (*List == NULL)
+        return;
+    OP *Now = *List;
     OP *Pre = NULL;
     while (Now != NULL)
-        if (!Now->HP)
-            if (*BulletList == Now) {
-                *BulletList = Now->Next;
+        if (!Now->HP) {
+            if (Now->Type & L1EType)
+                Level1Cnt--;
+            if (Now->Type & L2E2Type)
+                Level2Cnt--;
+            if (*List == Now) {
+                *List = Now->Next;
                 free(Now);
-                Now = *BulletList;
+                Now = *List;
             } else {
                 Pre->Next = Now->Next;
                 free(Now);
                 Now = Pre;
             }
-        else {
+        } else {
             Pre = Now;
             Now = Now->Next;
         }
+
 }
 
-void Cooldown() {
+void CoolDown() {
     MyBulletCD -= MyBulletCD > 0 ? 1 : 0;
+    ChargeCD -= ChargeCD > 0 ? 1 : 0;
+    LevelCD -= LevelCD > 0 ? 1 : 0;
+    TwinkTime -= TwinkTime > 0 ? 4 : -356;
+}
+
+double XY2Dir(float x1, float y1, float x2, float y2) {
+    return atan((double) ((x2 - x1) / (y2 - y1))) * R2D;
+}
+
+float XY2Dis(float x1, float y1, float x2, float y2) {
+    return sqrtf(powf(x2 - x1, 2) + powf(y2 - y1, 2));
+}
+
+void CreateEnemy(int level) {
+    if (ChargeEnemy == NULL)
+        ChargeCD = 0;
+    switch (level) {
+        case 1:
+            if (!ChargeCD) {
+                ChargeEnemyForm += ChargeEnemyForm < 3 ? 1 : -2;
+                ListAdd(&ChargeEnemy, CreateObject(My, L1EA, 4, 0, L1EHP, L1EType, 0, L1EDmg));
+                ListAdd(&ChargeEnemy, CreateObject(My, L1EA, 4, 0, L1EHP, L1EType, 0, L1EDmg));
+                ListAdd(&ChargeEnemy, CreateObject(My, L1EA, 4, 0, L1EHP, L1EType, 0, L1EDmg));
+                ChargeCD += 1000;
+            }
+            break;
+        case 2:
+            if (!ChargeCD) {
+                ChargeEnemyForm += ChargeEnemyForm < 5 ? 1 : -1;
+                ListAdd(&ChargeEnemy, CreateObject(My, L2EA, 5, 0, L2E2HP, L2E2Type, 0, L2E2Dmg));
+                ListAdd(&ChargeEnemy, CreateObject(My, L2EA, 5, 0, L2E2HP, L2E2Type, 0, L2E2Dmg));
+                ChargeCD += 1000;
+            }
+            break;
+        case 3:
+            if (!ChargeCD) {
+                ChargeEnemyForm += ChargeEnemyForm < 8 ? 1 : -2;
+                ListAdd(&ChargeEnemy, CreateObject(My, L3EA, 6, 0, L3EHP, L3EType, 0, L3EDmg));
+                ListAdd(&ChargeEnemy, CreateObject(My, L3EA, 6, 0, L3EHP, L3EType, 0, L3EDmg));
+                ChargeCD += 1000;
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
+void Move() {
+    ListMove(&MyBullets);
+    ListMove(&ChargeEnemy);
+    ListMove(&PeltEnemy);
+    Upgrade();
+
+
+}
+
+void Upgrade() {
+    if (!Level) {
+        Level = 1;
+        LevelCD = 300;
+        Level1Cnt = 36;
+        Level2Cnt = 12;
+        LevelCD = 150;
+        ChargeEnemyForm = 3;
+        ChargeRandRage = 400;
+    } else if (!Level1Cnt && Level == 1) {
+        Level = 2;
+        LevelCD = 150;
+        ChargeEnemyForm = 3;
+        ChargeRandRage = 200;
+    } else if (!Level2Cnt && Level == 2) {
+        Level = 3;
+        LevelCD = 150;
+        ChargeEnemyForm = 5;
+        ChargeRandRage = 100;
+    }
 }
 
 void PrintAnime() {
 //    SDL_RenderClear(Renderer);
+    PrintGameBG();
+    PrintList(&ChargeEnemy, EnemySurface, ChargeEnemyForm);
+    PrintList(&MyBullets, BulletSurface, 1);
+    PrintMyself();
 
-    PrintBG();
-
-    SDL_Texture *MyTexture = SDL_CreateTextureFromSurface(Renderer, My1Surface);
-    SDL_RenderCopyExF(Renderer, MyTexture, NULL, &My.FRect, 180 - My.Direction, &My.FCentre, 0);
-    SDL_DestroyTexture(MyTexture);
-
-    BulletListMove(&MyBullets);
-    for (OP *i = MyBullets; i != NULL; i = i->Next) {
-        SDL_Texture *MyBulletTexture = SDL_CreateTextureFromSurface(Renderer, MyBullet1Sureface);
-        SDL_RenderCopyExF(Renderer, MyBulletTexture, NULL, &i->FRect, -i->Direction, &i->FCentre, 0);
-        SDL_DestroyTexture(MyBulletTexture);
-    }
-
+    if (LevelCD)
+        PrintLevel();
     SDL_RenderPresent(Renderer);
 }
 
-void PrintBG() {
-    SDL_Texture *BackGroundTexture = SDL_CreateTextureFromSurface(Renderer, BackGroundSurface);
-    SDL_RenderCopy(Renderer, BackGroundTexture, NULL, &BackGroundRect);
-    SDL_DestroyTexture(BackGroundTexture);
-    BackGroundRect.y += BackGroundRect.y >= 0 ? -Height + ScrollSpeed : ScrollSpeed;
+void PrintThemeBG() {
+    SDL_Texture *ThemeBGTexture = SDL_CreateTextureFromSurface(Renderer, ThemeBGSurface);
+    SDL_RenderCopy(Renderer, ThemeBGTexture, NULL, &ThemeBGRect);
+    SDL_DestroyTexture(ThemeBGTexture);
+    SDL_Surface *ThemeTipsSurface = TTF_RenderUTF8_Blended(FontMiddle, ThemeTips, FontTwinkColor);
+    SDL_Texture *ThemeTipsTexture = SDL_CreateTextureFromSurface(Renderer, ThemeTipsSurface);
+    SDL_Rect ThemeTipsRect = {(Width - ThemeTipsSurface->w) / 2, 750, ThemeTipsSurface->w, ThemeTipsSurface->h};
+    SDL_RenderCopy(Renderer, ThemeTipsTexture, NULL, &ThemeTipsRect);
+    SDL_FreeSurface(ThemeTipsSurface);
+    SDL_DestroyTexture(ThemeTipsTexture);
+    SDL_RenderPresent(Renderer);
+    TwinkTime -= TwinkTime > 0 ? 4 : -356;
+    FontTwinkColor.a += (int) (9 * sin(TwinkTime * D2R));
 }
 
+void PrintGameBG() {
+    SDL_Texture *GameBGTexture = SDL_CreateTextureFromSurface(Renderer, GameBGSurface);
+    SDL_RenderCopy(Renderer, GameBGTexture, NULL, &GameBGRect);
+    SDL_DestroyTexture(GameBGTexture);
+    GameBGRect.y += GameBGRect.y >= 0 ? -Height + ScrollSpeed : ScrollSpeed;
+}
 
-void Load() {
+void PrintList(OP **List, SDL_Surface *ListSurface[], int Form) {
+    for (OP *i = *List; i != NULL; i = i->Next) {
+        SDL_Texture *ListTexture = SDL_CreateTextureFromSurface(Renderer, ListSurface[Form]);
+        SDL_RenderCopyExF(Renderer, ListTexture, NULL, &i->FRect, -i->Direction, &i->FCentre, 0);
+        SDL_DestroyTexture(ListTexture);
+    }
+}
+
+void PrintMyself() {
+    SDL_Texture *MyTexture = SDL_CreateTextureFromSurface(Renderer, MySurface[My.HP > MyHP * 0.6 ? 1 : My.HP > MyHP * 0.3 ? 2 : 3]);
+    SDL_RenderCopyExF(Renderer, MyTexture, NULL, &My.FRect, 180 - My.Direction, &My.FCentre, 0);
+    SDL_DestroyTexture(MyTexture);
+}
+
+void PrintLevel() {
+    char TitleLevel[20];
+    if (LevelCD > 150)
+        sprintf_s(TitleLevel, 10, "Start");
+    else
+        sprintf_s(TitleLevel, 10, "Level%d", Level);
+    SDL_Surface *TitleLevelSurface = TTF_RenderUTF8_Blended(FontLarge, TitleLevel, FontTwinkColor);
+    SDL_Texture *TitleLevelTexture = SDL_CreateTextureFromSurface(Renderer, TitleLevelSurface);
+    SDL_Rect TitleLevelRect = {(Width - TitleLevelSurface->w) / 2, (Height - TitleLevelSurface->h) / 2, TitleLevelSurface->w, TitleLevelSurface->h};
+    SDL_RenderCopy(Renderer, TitleLevelTexture, NULL, &TitleLevelRect);
+    SDL_FreeSurface(TitleLevelSurface);
+    SDL_DestroyTexture(TitleLevelTexture);
+    FontTwinkColor.a += (int) (8 * sin(TwinkTime * D2R));
+}
+
+void LoadRes() {
+    char FileName[40];
     BGM = Mix_LoadMUS("res/Enemy.mp3");
     Mix_VolumeMusic(BGMVolume);
-    BackGroundSurface = IMG_Load("res/BackGround.png");
-    My1Surface = IMG_Load("res/I1.png");
-    MyBullet1Sureface = IMG_Load("res/MyBullet1.png");
+
+    FontLarge = TTF_OpenFont("res/GenshinDefault.ttf", 64);
+    FontMiddle = TTF_OpenFont("res/GenshinDefault.ttf", 40);
+    FontSmall = TTF_OpenFont("res/GenshinDefault.ttf", 28);
+
+    GameBGSurface = IMG_Load("res/GameBG.png");
+    ThemeBGSurface = IMG_Load("res/ThemeBG.png");
+    for (int i = 1; i <= 5; ++i) {
+        sprintf_s(FileName, 30, "res/I%d.png", i);
+        MySurface[i] = IMG_Load(FileName);
+    }
+    for (int i = 1; i <= 15; ++i) {
+        sprintf_s(FileName, 30, "res/Enemy%d.png", i);
+        EnemySurface[i] = IMG_Load(FileName);
+    }
+    for (int i = 1; i <= 5; ++i) {
+        sprintf_s(FileName, 30, "res/Boss%d.png", i);
+        BossSurface[i] = IMG_Load(FileName);
+    }
+    for (int i = 1; i <= 10; ++i) {
+        sprintf_s(FileName, 30, "res/Bullet%d.png", i);
+        BulletSurface[i] = IMG_Load(FileName);
+    }
+    for (int i = 1; i <= 30; ++i) {
+        sprintf_s(FileName, 30, "res/Props%d.png", i);
+        PropsSurface[i] = IMG_Load(FileName);
+    }
 }
--- Sakiyary_EmojiBattle.h
+++ Sakiyary_EmojiBattle.h
@@ -14,14 +14,69 @@
 #define Width 1080
 #define Height 960
 #define D2R 0.01745329252
+#define R2D 57.29577951
 #define ScrollSpeed 10
-#define BGMVolume 20
-#define KDFPS 10
-#define MyA 96
-#define MyBulletA 64
+#define BGMVolume 30
+#define mSPF 10
 
+#define MyA 96.0
+#define L1EA 120.0
+#define L2EA 144.0
+#define L3EA 120.0
+#define L3BossA 216.0
+#define PropsA 120.0
 
-typedef struct ObjectParameters{
+#define MyBltA 64.0
+#define L2BltA 80.0
+#define L3Blt1A 80.0
+#define L3Blt2A 240.0
+
+#define MyHP 1000
+#define L1EHP 60
+#define L2E1HP 2000
+#define L2E2HP 400
+#define L3EHP 601
+#define L3BossHP 200000
+
+#define MyDmg 0
+#define MyBltDmg 100
+#define L1EDmg 200
+#define L2E1Dmg 1000
+#define L2BltDmg 100
+#define L2E2Dmg 300
+#define L3EDmg 400
+#define L3BossDmg 1000
+#define L3Blt1Dmg 200
+#define L3Blt2Dmg 200
+
+enum InputAction {
+    MoveUp = 0x0001,
+    MoveDown = 0x0002,
+    MoveLeft = 0x0004,
+    MoveRight = 0x0008,
+    Shoot = 0x0010,
+    RotateLeft = 0x0020,
+    RotateRight = 0x0040,
+    RotateReset = 0x0080,
+    Pause = 0x1000,
+    Tab = 0x2000,
+    Back2Theme = 0x8000
+};
+
+enum Type {
+    MyType = 0x0000,
+    BossType = 0x0001,
+    MyBltType = 0x0010,
+    L2BltType = 0x0020,
+    L3Blt1Type = 0x0040,
+    L3Blt2Type = 0x0080,
+    L1EType = 0x0100,
+    L2E1Type = 0x0200,
+    L2E2Type = 0x0400,
+    L3EType = 0x0800,
+};
+
+typedef struct ObjectParameters {
     SDL_FRect FRect;
     SDL_FPoint FCentre;
     float FSpeed;
@@ -31,40 +86,73 @@ typedef struct ObjectParameters{
     int Status;
     int Damage;
     struct ObjectParameters *Next;
-}OP;
+} OP;
 
-void InitialUI(SDL_Event *event,const Uint8 *KeyValue);
-int GameUI(SDL_Event *event,const Uint8 *KeyValue);
+void InitAll();
+void ThemeUI(SDL_Event *event, const Uint8 *KeyValue);
+int GameUI(SDL_Event *event, const Uint8 *KeyValue);
 int GetKey(const Uint8 *KeyValue);
 int MyAction(int Input);
-void ListPush(OP **List,OP *NewNode);
-OP *CreateBullet(OP who, int type,float size, float fspeed,double direction,int damage);
-void BulletListMove(OP **BulletList);
-void BulletListRemove(OP **BulletList);
-void Cooldown();
-void Load();
+void ListAdd(OP **List, OP *NewNode);
+OP *CreateObject(OP who, float size, float fspeed, double direction, int hp, int type, int status, int damage);
+void Collision(OP **List);
+void ListMove(OP **List);
+void ListRemove(OP **List);
+void CoolDown();
+double XY2Dir(float x1, float y1, float x2, float y2);
+float XY2Dis(float x1, float y1, float x2, float y2);
+void CreateEnemy(int level);
+void Move();
+void Upgrade();
+void LoadRes();
 void PrintAnime();
-void PrintBG();
+void PrintThemeBG();
+void PrintGameBG();
+void PrintList(OP **List, SDL_Surface *ListSurface[], int Form);
+void PrintMyself();
+void PrintLevel();
 
 static SDL_Window *Window = NULL;
 static SDL_Renderer *Renderer = NULL;
 static SDL_Event TotleEvent;
 
-static Mix_Music *BGM=NULL;
-static Mix_Chunk *Boom=NULL;
+static Mix_Music *BGM = NULL;
+static Mix_Chunk *Boom = NULL;
 
-static SDL_Surface *BackGroundSurface=NULL;
-static SDL_Surface *My1Surface=NULL;
-static SDL_Surface *MyBullet1Sureface=NULL;
+static TTF_Font *FontLarge = NULL;
+static TTF_Font *FontMiddle = NULL;
+static TTF_Font *FontSmall = NULL;
+static SDL_Color FontTwinkColor = {0, 0, 0, 0xFF};
 
-static SDL_Rect BackGroundRect={0,-Height,Width,Height*2};
-static OP My={{(float)(Width-MyA)/2, (float)Height/4*3, MyA, MyA},
-              {(float)MyA/2,(float)MyA/2},10, 180,
-              1000,0x0,0x0,0,NULL};
-static OP *MyBullets=NULL;
+static SDL_Surface *ThemeBGSurface = NULL;
+static SDL_Surface *GameBGSurface = NULL;
+static SDL_Surface *MySurface[10] = {NULL};
+static SDL_Surface *EnemySurface[20] = {NULL};
+static SDL_Surface *BossSurface[10] = {NULL};
+static SDL_Surface *BulletSurface[10] = {NULL};
+static SDL_Surface *PropsSurface[30] = {NULL};
 
-static int MyBulletCD;
+static SDL_Rect ThemeBGRect = {0, 0, Width, Height};
+static SDL_Rect GameBGRect = {0, -Height, Width, Height * 2};
 
+static OP My = {{(float) (Width - MyA) / 2, (float) Height / 5 * 4, MyA, MyA},
+                {(float) MyA / 2, (float) MyA / 2}, 10, 180,
+                MyHP, MyType, 0x0, MyDmg, NULL};
+static OP *MyBullets = NULL;
+static OP *ChargeEnemy = NULL;
+static OP *PeltEnemy = NULL;
+static OP *Boss = NULL;
 
+static int MyBulletCD, ChargeCD, LevelCD;
+static int RorL;
+static int Level, Level1Cnt, Level2Cnt;
+static int ChargeEnemyForm;
+static int ChargeRandRage;
+
+static int TwinkTime = 360;
+static char ThemeTips[] = "Press_\"Enter\"_to_Start!";
+static char PauseTips[] = "Press_\"Space\"_to_Continue!";
+static char WinTips[] = "Press_\"Enter\"_to_Restart!";
+static char LoseTips[] = "Press_\"Enter\"_to_Remake!";
 
 #endif //SAKIYARY_EMOJIBATTLE_C_SAKIYARY_EMOJIBATTLE_H
